6846e9afd1593e772640fd16a90aee781ba28d07
chandu-kurva <saee1604@gmail.com>
2021-04-03 09:02:47 +0000

    hss

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let factorialOfNumber = require("../factorial/index.js");
-let ratioOfTwoNumbers = require("../ratio/index.js");
+let factorialOfNumber = require("../factorial/index");
+let ratioOfTwoNumbers = require("../ratio/index");
 
 let ratioAndFactorial = function (num1, num2, num3) {
   let ratio = ratioOfTwoNumbers(num1, num2);
